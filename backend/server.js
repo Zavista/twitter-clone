0b68aa4cef8ef11cef72f450bf36af9c11b0fc65
Zavista <84294353+Zavista@users.noreply.github.com>
2024-08-18 14:40:16 -0400

    added express middleware

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,13 @@ import authRoutes from "./routes/auth.routes.js";
 import dotenv from "dotenv";
 import connectMongoDB from "./db/connectMongoDB.js";
 
+const PORT = process.env.PORT || 3000;
+
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
 app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
