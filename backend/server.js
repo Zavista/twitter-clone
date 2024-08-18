d258e3ab303e850ab88ef93f8e00f53e6de98742
Zavista <84294353+Zavista@users.noreply.github.com>
2024-08-18 14:10:03 -0400

    mongoDB Setupt

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
+import dotenv from "dotenv";
+import connectMongoDB from "./db/connectMongoDB.js";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use("/api/auth", authRoutes);
 
@@ -9,6 +14,7 @@ app.get("/", (req, res) => {
   res.send("Server is ready");
 });
 
-app.listen(8000, () => {
-  console.log("Server is running on port 8000");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectMongoDB();
 });
