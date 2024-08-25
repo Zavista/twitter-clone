9f3c6a519856656dbfdc5a19e08cf059646f3ea6
Zavista <84294353+Zavista@users.noreply.github.com>
2024-08-25 16:22:25 -0400

    UI design setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,29 @@ import { Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/auth/login/LoginPage";
 import HomePage from "./pages/home/HomePage";
 import SignUpPage from "./pages/auth/signup/SignUpPage";
+import Sidebar from "./components/common/Sidebar";
+import RightPanel from "./components/common/RightPanel";
+import NotificationPage from "./pages/notification/NotificationPage";
+import ProfilePage from "./pages/profile/ProfilePage";
 
 function App() {
   return (
     <div className="flex max-w-6xl mx-auto">
+      <Sidebar></Sidebar>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/notifications"
+          element={<NotificationPage></NotificationPage>}
+        />
+        <Route
+          path="/profile/:username"
+          element={<ProfilePage></ProfilePage>}
+        />
       </Routes>
+      <RightPanel></RightPanel>
     </div>
   );
 }
---
+++ frontend/src/components/common/LoadingSpinner.tsx
@@ -0,0 +1,10 @@
+type LoadingSpinnerProps = {
+  size: string;
+};
+
+const LoadingSpinner = ({ size = "md" }: LoadingSpinnerProps) => {
+  const sizeClass = `loading-${size}`;
+
+  return <span className={`loading loading-spinner ${sizeClass}`} />;
+};
+export default LoadingSpinner;
