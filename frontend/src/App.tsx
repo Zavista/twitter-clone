d7d8ab023216fc299d71d1d78f788acc54f5b3e5
Zavista <84294353+Zavista@users.noreply.github.com>
2024-08-25 12:36:17 -0400

    React Router setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/auth/login/LoginPage";
+import HomePage from "./pages/home/HomePage";
+import SignUpPage from "./pages/auth/signup/SignUpPage";
+
 function App() {
   return (
-    <>
-      <p>Hello</p>
-    </>
+    <div className="flex max-w-6xl mx-auto">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </div>
   );
 }
 
