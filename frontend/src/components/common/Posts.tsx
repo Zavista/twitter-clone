91b811426a5f1e209ddc063e12c2b48cb6195d49
Zavista <84294353+Zavista@users.noreply.github.com>
2024-08-25 18:43:13 -0400

    added get posts logic

--- frontend/src/components/common/Posts.tsx
+++ frontend/src/components/common/Posts.tsx
@@ -1,25 +1,66 @@
 import Post from "./Post";
 import PostSkeleton from "../skeletons/PostSkeleton";
 import { POSTS } from "../../utils/db/dummy";
+import { useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
 
-const Posts = () => {
-  const isLoading = false;
+type PostsProps = {
+  feedType: string;
+};
+
+const Posts = ({ feedType }: PostsProps) => {
+  const { data, isLoading, refetch, isRefetching } = useQuery({
+    queryKey: ["posts"],
+    queryFn: async () => {
+      try {
+        let res;
+        let data;
+
+        switch (feedType) {
+          case "for you":
+            res = await fetch("/api/post/all");
+            data = await res.json();
+            break;
+          case "following":
+            res = await fetch("/api/post/following");
+            data = await res.json();
+            break;
+          default:
+            res = await fetch("/api/post/all");
+            data = await res.json();
+            break;
+        }
+
+        return data;
+
+        if (!res.ok) {
+          throw new Error(data.error || "Something went wrong");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  });
+
+  useEffect(() => {
+    refetch();
+  }, [feedType, refetch]);
 
   return (
     <>
-      {isLoading && (
+      {(isLoading || isRefetching) && (
         <div className="flex flex-col justify-center">
           <PostSkeleton />
           <PostSkeleton />
           <PostSkeleton />
         </div>
       )}
-      {!isLoading && POSTS?.length === 0 && (
+      {!isLoading && !isRefetching && data?.length === 0 && (
         <p className="text-center my-4">No posts in this tab. Switch 👻</p>
       )}
-      {!isLoading && POSTS && (
+      {!isLoading && !isRefetching && data && (
         <div>
-          {POSTS.map((post) => (
+          {data.map((post) => (
             <Post key={post._id} post={post} />
           ))}
         </div>
